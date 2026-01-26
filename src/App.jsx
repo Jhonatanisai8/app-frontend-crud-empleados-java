bb64d1b291e36ab9b89a69fecb75158061848ea2
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-25 23:44:46 -0500

    creando un componente para listar data en una tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import HolaMundo from "./HolaMundo.jsx";
+import ListaEmpleados from "./components/ListaEmpleados.jsx";
 
 function App() {
 
   return (
     <div className="App">
-      <HolaMundo></HolaMundo>
+      <ListaEmpleados></ListaEmpleados>
     </div>
   )
 }
---
+++ src/components/ListaEmpleados.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const ListaEmpleados = () => {
+
+  const miData = [
+    {
+      "idEmpleado": 1,
+      "nombre": "Samir Elias",
+      "apellido": "Correa Salvador",
+      "email": "samirelias@gmail.com",
+    },
+    {
+      "idEmpleado": 2,
+      "nombre": "Elvis Alonso",
+      "apellido": "Correa Salvador",
+      "email": "elvisalonso@gmail.com",
+    },
+    {
+      "idEmpleado": 3,
+      "nombre": "Diana Susena",
+      "apellido": "Correa Salvador",
+      "email": "dianasusena@gmail.com",
+    }
+  ]
+  return (
+    <div className="container">
+      <h2>Lista de Empleados:</h2>
+      <table className="table">
+        <thead>
+        <tr>
+          <td>ID</td>
+          <td>NOMBRE</td>
+          <td>APELLIDO</td>
+          <td>EMAIL</td>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          miData.map(empleado =>
+            <tr key={empleado.idEmpleado}>
+              <td>{empleado.idEmpleado}</td>
+              <td>{empleado.nombre}</td>
+              <td>{empleado.apellido}</td>
+              <td>{empleado.email}</td>
+            </tr>)
+        }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ListaEmpleados
