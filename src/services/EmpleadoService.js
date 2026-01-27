170357d86bd8fc382962bce41253d4b839b862c3
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-26 22:55:34 -0500

    corrigiendo el puerto de nuestra url api

--- src/services/EmpleadoService.js
+++ src/services/EmpleadoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const REST_API_BASE_URL = 'http://localhost:8000/api/v1/empleados'
+const REST_API_BASE_URL = 'http://localhost:8080/api/v1/empleados'
 export const listaEmpleados = () => {
   return axios.get(REST_API_BASE_URL)
 }
