8bfe9f645d3b8c631fc6ec9910366002278230ce
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-26 23:20:48 -0500

    agregando nuestro componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HolaMundo from "./HolaMundo.jsx";
 import ListaEmpleados from "./components/ListaEmpleados.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
     <div className="App">
+      <Header></Header>
       <ListaEmpleados></ListaEmpleados>
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className=" navbar bg-dark border-bottom text-center border-body" data-bs-theme="dark">
+      <a className="navbar-brand text-uppercase fw-semibold fs-1" href="#">Mi app React + Java</a>
+    </nav>
+  )
+}
+
+export default Header
