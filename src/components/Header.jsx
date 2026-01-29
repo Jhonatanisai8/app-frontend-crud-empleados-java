c88cdd3c15d124c719520e7c2b0a14201a9bc342
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-28 21:38:13 -0500

    implementacion del centrado de nuestro contenido de Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <nav className=" navbar bg-dark border-bottom text-center border-body" data-bs-theme="dark">
-      <a className="navbar-brand text-uppercase fw-semibold fs-1" href="#">Mi app React + Java</a>
+    <nav className="navbar bg-dark border-bottom border-body justify-content-center" data-bs-theme="dark">
+      <a className="texto-title navbar-brand text-uppercase fw-semibold fs-1" href="#">Mi app React + Java</a>
     </nav>
+
   )
 }
 
