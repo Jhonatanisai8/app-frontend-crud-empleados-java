75bb51cacc6ce12433e97ac91389366473d79025
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-30 23:23:26 -0500

    implementacion de la segunda version de form para el registro de Empleado.jsx

--- src/components/Empleado.jsx
+++ src/components/Empleado.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Empleado = () => {
+  const [nombre, setNombre] = useState()
+  const [apellido, setApellido] = useState()
+  const [email, setEmail] = useState()
   return (
-    <div className="container ms-auto">
-      <h1 className="fs-1 text-center">Completar los datos del empleado</h1>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Nombres:</label>
-          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="card ">
+          <div className="text-center card-header">
+            DATOS DEL EMPLEADO
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">REGISTRO DE EMPLEADO</h5>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Nombres</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/>
+              </div>
+              <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+          <div className="card-footer text-muted">
+            2 days ago
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Apellidos:</label>
-          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email:</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-        </div>
-        <button type="submit" className=" btn btn-primary">Enviar</button>
-      </form>
+      </div>
     </div>
   )
 }
