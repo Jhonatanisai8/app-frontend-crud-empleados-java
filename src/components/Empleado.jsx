bbec4be553ed19413a6bc374958f9f8db0325309
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-30 23:06:58 -0500

    implementacion de la creacion del formulario para agregar el empleado

--- src/components/Empleado.jsx
+++ src/components/Empleado.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 const Empleado = () => {
   return (
-    <div className="container">
-      <p>Hola</p>
+    <div className="container ms-auto">
+      <h1 className="fs-1 text-center">Completar los datos del empleado</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Nombres:</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Apellidos:</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email:</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <button type="submit" className=" btn btn-primary">Enviar</button>
+      </form>
     </div>
   )
 }
